9981f613d94e176ef399dd038fae5c89d4ea6cde
Erwin <afanouerwin40@gmail.com>
2026-08-18 07:38:41 +0000

    fix(config):pointe vers API deployer

--- config.js
+++ config.js
@@ -11,4 +11,4 @@
      (ex : "https://portfolio-api.onrender.com") puis redéploie ce site.
    ================================================================ */
 
-var PORTFOLIO_API_URL = "http://localhost:4000";
+var PORTFOLIO_API_URL = "https://erwin-dev-api.onrender.com";
